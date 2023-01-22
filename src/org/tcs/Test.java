bff7d51894fe0d646cd13af68dbb412ff31ced63
AbishekAV <abishekabi89@gmail.com>
2023-01-22 19:45:15 +0530

    New task

---
+++ src/org/tcs/Test.java
@@ -0,0 +1,17 @@
+package org.tcs;
+
+
+public class Test {
+	int a = 4;
+	int b= 66;
+	 int c = a+b;
+	 private void asdfg() {
+		 System.out.println(c);
+		  
+		
+
+	}
+public static void main(String[] args) {
+	
+}
+}
